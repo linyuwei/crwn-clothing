f0c7db191ccccc79cee63798abe1ffcdf4fc7b69
linyuwei <linyuwei.designer@gmail.com>
2022-04-12 22:38:49 +0800

    build router

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,29 @@
+import { Fragment } from 'react'
+import { Outlet, Link } from 'react-router-dom'
+
+import { ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+
+import './navigation.styles.scss'
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <CrwnLogo className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            <h1>Shop</h1>
+          </Link>
+          <Link className='nav-link' to='/sign-in'>
+            <h1>Sign In</h1>
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  )
+}
+
+export default Navigation
