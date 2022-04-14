1693dd56ae6796f0c5216caca6f3f7043db21ecb
linyuwei <linyuwei.designer@gmail.com>
2022-04-14 18:27:01 +0800

    Created user context component and installed it in the application

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.scss';
 import App from './App';
+import { UserProvider } from './contexts/user.context.jsx'
+
+import './index.scss';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>      
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,22 @@
-import { Fragment } from 'react'
+import { Fragment, useContext } from 'react'
 import { Outlet, Link } from 'react-router-dom'
 
 import { ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+import { UserContext } from '../../contexts/user.context'
+
+import { signOutUser } from '../../utils/firebase/firebase.utils'
 
 import './navigation.styles.scss'
 
 const Navigation = () => {
+  const { currentUser, setCurrentUser } = useContext(UserContext)
+  // console.log(currentUser)
+
+  const signOutHandler = async () => {
+    await signOutUser()
+    setCurrentUser(null)
+  }
+
   return (
     <Fragment>
       <div className='navigation'>
@@ -16,9 +27,13 @@ const Navigation = () => {
           <Link className='nav-link' to='/shop'>
             <h1>Shop</h1>
           </Link>
-          <Link className='nav-link' to='/auth'>
-            <h1>Sign In</h1>
-          </Link>
+          { currentUser ? (
+            <span className='nav-link' onClick={signOutHandler}>Sign Out</span>
+          ) : (
+            <Link className='nav-link' to='/auth'>
+              <h1>Sign In</h1>
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
