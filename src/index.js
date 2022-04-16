eb5ec092496199659c82382948c57f8d6aa58472
linyuwei <linyuwei.designer@gmail.com>
2022-04-16 17:16:31 +0800

    Created shop page and shop-data context.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import { UserProvider } from './contexts/user.context.jsx'
+import { UserProvider } from './contexts/user.context'
+import { ProductsProvider } from './contexts/products.context'
 
 import './index.scss';
 
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const rooElement = document.getElementById('root');
+
+render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
-      </UserProvider>      
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
